91b8f914040f73fed02abb7ffde230a39b8157eb
GreyA <grey.altaer@gmail.com>
2025-01-26 21:20:06 -0800

    added forEach function for all buttons

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
 const buttons = document.querySelectorAll('.button');
 
+buttons.forEach((button) => {
+    button.addEventListener('click'), (event) => {
+        console.log(event.target.innerText);
+    }
+})
+
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
