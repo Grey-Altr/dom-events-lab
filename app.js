e52b7136fa70e49e691a1fff1230e15cad19e029
GreyA <grey.altaer@gmail.com>
2025-01-30 07:59:56 -0800

    empty variables declared for number entries and operations

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 /*-------------------------------- Constants --------------------------------*/
+
 const numButtons = document.querySelectorAll('.button.number');
 const opButtons = document.querySelectorAll('.button.operator');
 const eqButton = document.querySelector('.button.equals');
 const clearButton = document.querySelector('.button.operator.clear');
 const display = document.querySelector('.display');
 
-
 /*-------------------------------- Variables --------------------------------*/
+
+let firstNum = '';
+let secNum = '';
+let operator = '';
 
 /*------------------------ Cached Element References ------------------------*/
 
